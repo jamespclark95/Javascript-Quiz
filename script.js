bce7724ccb4ac0ab380753b5adcdda09b5300043
James Clark <clarkey95@hotmail.com>
2021-04-02 09:39:41 -0400

    restructured code

--- script.js
+++ script.js
@@ -1,17 +1,18 @@
+// questions & answers array
 let questions = [
     {
       question: 'What does NaN stand for?', 
-      choices:['Never Ask Names', 'Nod at Nanna', 'New and Neat', 'Not a Number'],
+      choices:['Not a Number', 'Nod at Nanna', 'New and Neat', 'Never Ask Names'],
       answer: 'Not a Number'
     },
     {
       question: 'What was JavaScript originally named?', 
-      choices:['CoffeeScript', 'CaramelScript', 'Java', 'Mocha'],
+      choices:['CoffeeScript', 'CaramelScript', 'Mocha', 'Java'],
       answer: 'Mocha'
     },
     {
-      question: 'How many items can an array hold?', 
-      choices:['One', 'Infinite', 'Thirty', '4.29 billion'],
+      question: 'How many items can an array hold before it starts slowing down?', 
+      choices:['One', '4.29 billion', 'Thirty', 'Infinite'],
       answer: '4.29 billion'
     },
     {
@@ -21,50 +22,75 @@ let questions = [
     }
   ];
 
-
-  let button = document.getElementById("button")
-  let startBox = document.querySelector('.startbox')
-  let quizWrapper = document.querySelector(".quiz-wrapper")
-  let qIndex = 0
-  let questionDisplay = document.getElementById("question")
-  let answerList = document.getElementById('answerList')
-
-
-    button.addEventListener('click', function (){
-    buildQuestionCard();
-
-    startBox.setAttribute('style', "display: none;")
-
-    quizWrapper.removeAttribute('style')
-    })
-
-    let buildQuestionCard = ()=> {
-        questionDisplay.innerHTML = ''
-        answerList.innerHTML = ''
-        questionDisplay.textContent = questions[qIndex].question
-
-        questions[qIndex].choices.forEach(function(choice){
-            let answerButton = document.createElement('button');
-            answerButton.textContent = choice;
-            answerButton.setAttribute('value', choice);
-            answerButton.onclick = evaluateAnswer;
-            answerList.appendChild(answerButton)
-        })
+  // created variables for necessary classes, elements & array index
+let button = document.getElementById("button")
+let startBox = document.querySelector('.startbox')
+let quizWrapper = document.querySelector(".quiz-wrapper")
+let qIndex = 0
+let questionDisplay = document.getElementById("question")
+let timerEl = document.getElementById('secondsLeft')
+let secondsLeft = 60
+let answerList = document.getElementById('answerList')
+let points = 0
+  
+function startTimer () {
+  let countDown = setInterval(() => {
+    secondsLeft--;
+    timerEl.textContent = secondsLeft
+    if (secondsLeft === 0) {
+      alert('Your time has run out. Refresh and try again!')
+      clearInterval(countDown)
+    }
+  }, 1000);
+}
 
 
-      }
+
+// calls the buildQuestion function on each click
+button.addEventListener('click', function (){
+buildQuestionCard();
+startTimer();
+
+startBox.setAttribute('style', "display: none;")
+
+quizWrapper.removeAttribute('style')
+})
+
+let buildQuestionCard = ()=> {
+    
+// display new questions & answers based on the question index
+questionDisplay.innerHTML = ''
+answerList.innerHTML = ''
+questionDisplay.textContent = questions[qIndex].question
+
+  questions[qIndex].choices.forEach(function(choice){
+  let answerButton = document.createElement('button');
+  answerButton.textContent = choice;
+  answerButton.setAttribute('value', choice);
+  answerButton.onclick = evaluateAnswer;
+  answerList.appendChild(answerButton)
+  })
+}
 
 function evaluateAnswer() {
     if (this.value !== questions[qIndex].answer){
-        console.log('wrong')
+        secondsLeft = secondsLeft - 10
     }
-    else {console.log('correct')}
+    else {points = points + 25}
     qIndex++;
     if (qIndex === questions.length) {
-        console.log('game over')
+      gameOver()
     }
     else {buildQuestionCard()}
 }
 
+
+function gameOver() {
+  quizWrapper.setAttribute('style', "display: none;")
+  let div = document.createElement('div');
+  div.textContent = 'You finished the quiz. Your score is ' + points + '/100'
+  div.classList.add('gameover-message')
+  document.body.appendChild(div)
+}
 
 
